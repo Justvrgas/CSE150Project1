b7764928144c57017b27f8375c15b9db4e5536bc
Dexter Yesilkaya <dexteryesilkaya@Dexters-MBP.attlocal.net>
2019-10-28 21:15:09 -0700

    part6

--- nachos/threads/Boat.java
+++ nachos/threads/Boat.java
@@ -10,9 +10,21 @@ import nachos.ag.BoatGrader;
 
 public class Boat
 {
+	//declaring variables
     static BoatGrader bg;
+    static Lock lockObject;
+    static Condition2 adltAtOahu;
+    static String bPlace;
+    static int data;
+    static int adultsatOahu;
+    static int adultsatMolokai;
     
-    public static void selfTest()
+    
+    //testing 
+    static boolean showResult = false;
+    static boolean flagObject = true;
+    
+public static void selfTest()
     {
 	BoatGrader b = new BoatGrader();
 	
@@ -25,28 +37,55 @@ public class Boat
 //  	System.out.println("\n ***Testing Boats with 3 children, 3 adults***");
 //  	begin(3, 3, b);
     }
-
+    
+    
     public static void begin( int adults, int children, BoatGrader b )
     {
 	// Store the externally generated autograder in a class
 	// variable to be accessible by children.
 	bg = b;
+	bPlace = "Oahu";
 
 	// Instantiate global variables here
 	
 	// Create threads here. See section 3.4 of the Nachos for Java
 	// Walkthrough linked from the projects page.
 
-	Runnable r = new Runnable() {
+	Runnable c = new Runnable() 
+	{
 	    public void run() {
-                SampleItinerary();
+                ChildItinerary();
             }
         };
-        KThread t = new KThread(r);
-        t.setName("Sample Boat Thread");
-        t.fork();
-
+        if (showResult)
+        {
+        	System.out.println("Created Child Itinerary");
+        }
+       
+    Runnable a = new Runnable()
+    {
+    	 public void run() {
+    		 AdultItinerary();
+    	 }
+    		 };
+    	if (showResult)
+    	{
+    		System.out.println("Created Adult Itinerary");
+    	}
+    		 
+        for (int i=1; i<= children; i++) {
+        	
+        KThread child = new KThread(c);
+        child.setName("Child: " + i);
+        if (showResult)
+        {
+        	System.out.println("Child " + i + " created");
+        }
+        child.fork();
     }
+        
+        
+        
 
     static void AdultItinerary()
     {
@@ -56,10 +95,35 @@ public class Boat
 	       bg.AdultRowToMolokai();
 	   indicates that an adult has rowed the boat across to Molokai
 	*/
+    	lockObject.acquire();
+    	adultsatOahu = adultsatOahu +1;
+    	if (showResult)
+    	{
+    		System.out.println("Adult went to sleep on Oahu");
+    	}
+    	adltAtOahu.sleep();
+    	if (showResult)
+    	{
+    		System.out.println("Adult woke up");
+    	}
+    	if (flagObject)
+    	{
+    		bg.AdultRowToMolokai();
+    	}
+    	if (showResult)
+    	{
+    		System.out.println("Adult rowed to Molokai");
+    	}
+    	adultsAtOahu = AdultsAtOahu -1;
+    	bPlace = "Molokai";
+    	adultsAt
+    	
+    	
     }
 
     static void ChildItinerary()
     {
+    	boat_permit.acquire();
     }
 
     static void SampleItinerary()
